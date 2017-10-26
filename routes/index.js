c20a380cafda5aeff0d9ec62bfa99139910f4bf1
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-26 17:04:22 -0600

    Serve layout.pug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.render('hello', {
+    name: 'Wes'
+  });
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse)
+})
 
 module.exports = router;
